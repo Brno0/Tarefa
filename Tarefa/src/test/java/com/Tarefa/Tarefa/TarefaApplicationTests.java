8a2220d7b125d4f74e05407372b0360aba366aa9
Bruno Torres <torresbruno674@gmail.com>
2026-03-23 18:34:57 -0300

    Commit Inicial

---
+++ Tarefa/src/test/java/com/Tarefa/Tarefa/TarefaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tarefa.Tarefa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarefaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
